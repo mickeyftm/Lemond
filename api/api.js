f237f53d4188d9e4d94a6d4424461d5144c55a08
MacPhersonui <MacPhersonui>
2021-08-03 21:46:32 +0800

    commit

--- api/api.js
+++ api/api.js
@@ -27,7 +27,7 @@ export const getLemdPrice = async () => {
                 "query": "fragment PairFields on Pair {\n  id\n  txCount\n  token0 {\n    id\n    symbol\n    name\n    totalLiquidity\n    derivedETH\n    __typename\n  }\n  token1 {\n    id\n    symbol\n    name\n    totalLiquidity\n    derivedETH\n    __typename\n  }\n  reserve0\n  reserve1\n  reserveUSD\n  totalSupply\n  trackedReserveETH\n  reserveETH\n  volumeUSD\n  untrackedVolumeUSD\n  token0Price\n  token1Price\n  createdAtTimestamp\n  __typename\n}\n\nquery pairs($allPairs: [Bytes]!) {\n  pairs(where: {id_in: $allPairs}, orderBy: trackedReserveETH, orderDirection: desc) {\n    ...PairFields\n    __typename\n  }\n}\n"
             }
         )
-        const price = request?.data?.data?.pairs[0]?.token0Price
+        const price = request?.data?.data?.pairs[0]?.token1Price
     return !!price ? price : 0
 }
 
